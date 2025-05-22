94e5c91248d97c26be32ebaea9d127c25d0c95f3
scott-carnall <65339078+scott-carnall@users.noreply.github.com>
2025-05-22 15:35:57 +0100

    Add files via upload

---
+++ js/cart.js
@@ -0,0 +1,19 @@
+function getCart() {
+  return JSON.parse(localStorage.getItem('cart')) || [];
+}
+
+function saveCart(cart) {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function addToCart(product) {
+  const cart = getCart();
+  const existing = cart.find(item => item.id === product.id);
+  if (existing) {
+    existing.qty += 1;
+  } else {
+    product.qty = 1;
+    cart.push(product);
+  }
+  saveCart(cart);
+}
